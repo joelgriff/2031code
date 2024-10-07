7b630b2cf92d91a2236b5c4b0e24218bbef7bcbb
joelgriff <145991033+joelgriff@users.noreply.github.com>
2024-10-07 11:38:11 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+from flask import Flask, url_for
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.menu import MenuLink
+import secrets
+
+app = Flask(__name__)
+
+# SECRET KEY FOR FLASK FORMS
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+
+# DATABASE CONFIGURATION
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///csc2031blog.db'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+metadata = MetaData(
+    naming_convention={
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+    }
+)
+
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db)
+
+# DATABASE TABLES
+class Post(db.Model):
+   __tablename__ = 'posts'
+
+   id = db.Column(db.Integer, primary_key=True)
+   created = db.Column(db.DateTime, nullable=False)
+   title = db.Column(db.Text, nullable=False)
+   body = db.Column(db.Text, nullable=False)
+
+   def __init__(self, title, body):
+       self.created = datetime.now()
+       self.title = title
+       self.body = body
+
+# DATABASE ADMINISTRATOR
+class MainIndexLink(MenuLink):
+    def get_url(self):
+        return url_for('index')
+
+
+class PostView(ModelView):
+    column_display_pk = True
+    column_hide_backrefs = False
+    column_list = ('id', 'created', 'title', 'body')
+
+admin = Admin(app, name='DB Admin', template_mode='bootstrap4')
+admin._menu = admin._menu[1:]
+admin.add_link(MainIndexLink(name='Home Page'))
+admin.add_view(PostView(Post, db.session))
+
+## IMPORT BLUEPRINTS
+from accounts.views import accounts_bp
+from posts.views import posts_bp
+from security.views import security_bp
+
+## REGISTER BLUEPRINTS
+
+app.register_blueprint(accounts_bp, name="accounts")
+app.register_blueprint(posts_bp, name="posts")
+app.register_blueprint(security_bp, name="security")
