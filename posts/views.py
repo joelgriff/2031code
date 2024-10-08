39e81ed773b6a2129ff98fceec52ab157f00b028
joelgriffin53 <c3018412@newcastle.ac.uk>
2024-10-08 13:08:13 +0100

    begin create post (5)

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, url_for, redirect
+from config import db, Post
+from posts.forms import PostForm
 
 posts_bp = Blueprint('posts', __name__, template_folder='templates')
 
 @posts_bp.route('/create')
 def create():
-    return render_template('posts/create.html')
+
+    form = PostForm()
+
+    if form.validate_on_submit():
+        new_post = Post(title=form.title.data, body=form.body.data)
+
+        db.session.add(new_post)
+        db.session.commit()
+        flash('Post created', category='success')
+
+        return redirect(url_for('posts.posts'))
+
+    return render_template('posts/create.html', form=form)
 
 @posts_bp.route('/posts')
 def posts():
